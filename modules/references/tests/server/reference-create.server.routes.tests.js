20e7b89f9ee04b4e80addbfa58231186e5d37d1e
mrkvon <mrkvon@users.noreply.github.com>
2018-10-23 01:44:35 +0200

    read references: further works
    - format response body
    - populate userFrom and userTo
    - respond with all public references to userTo
    - refactor controllers
    - return created Date as ISOString, to be consistent with other endpoints

--- modules/references/tests/server/reference-create.server.routes.tests.js
+++ modules/references/tests/server/reference-create.server.routes.tests.js
@@ -90,14 +90,13 @@ describe('Create a reference', function () {
                 public: false,
                 userFrom: user1._id.toString(),
                 userTo: user2._id.toString(),
-                created: Date.now(),
+                created: new Date().toISOString(),
                 met: true,
                 hostedMe: true,
                 hostedThem: true,
-                recommend: 'yes'
+                recommend: 'yes',
+                _id: /^[0-9a-f]{24}$/
               });
-
-              should(res.body).have.property('id').match(/^[0-9a-f]{24}$/);
 
               return done();
             });
